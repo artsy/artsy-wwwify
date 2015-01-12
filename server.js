872a75664b6ced5542dfe0e050c10b2297790bf1
Craig Spaeth <craigspaeth@Craigs-iMac-2.local>
2015-01-12 13:29:08 -0500

    setup for OW

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 app.use(function(req, res, next) {
   res.setHeader('Strict-Transport-Security', 'max-age=0');
   res.redirect(301, 'https://www.artsy.net' + req.url);
