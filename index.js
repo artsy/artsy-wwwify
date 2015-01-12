f85edb1cc02e03c0a2436956e6c95cb6eefb9e6a
Craig Spaeth <craigspaeth@Craigs-iMac-2.local>
2015-01-12 12:32:32 -0500

    set header and start script

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 app.use(function(req, res, next) {
+  res.setHeader('Strict-Transport-Security', 'max-age=0');
   res.redirect(301, 'https://www.artsy.net' + req.url);
 });
 app.listen(port, function() {
