44cfef912c00f6a6d51db90be8149c1c6e13de39
Craig Spaeth <craigspaeth@Craigs-iMac-2.local>
2015-01-12 17:16:20 -0500

    try just http for now

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var https = require('https');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
-var options = {
-  key: fs.readFileSync(__dirname + '/shared/config/ssl.key'),
-  cert: fs.readFileSync(__dirname + '/shared/config/ssl.crt')
-};
 var app = express();
 app.use(function(req, res, next) {
   res.setHeader('Strict-Transport-Security', 'max-age=0');
   res.redirect(301, 'https://www.artsy.net' + req.url);
 });
 http.createServer(app).listen(80);
-https.createServer(options, app).listen(443);
+return
+https.createServer({
+  key: fs.readFileSync(__dirname + '/shared/config/ssl.key'),
+  cert: fs.readFileSync(__dirname + '/shared/config/ssl.crt')
+}, app).listen(443);
