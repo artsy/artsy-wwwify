04b2daad7da2645c1c2fc8f70c8ef72f3d89d6ff
Joey Aghion <joey@aghion.com>
2019-11-07 17:45:28 -0500

    Add hokusai and tests, remove SSL and env considerations

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 var artsyEigenWebAssociation = require('artsy-eigen-web-association');
 var express = require('express');
-var https = require('https');
 var http = require('http');
 var morgan = require('morgan');
-var path = require('path');
-var fs = require('fs');
 var app = express();
-var nodeEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'production'
-var port = nodeEnv == 'production' ? 80 : 3000
-var ssl = nodeEnv == 'production'
 app.use(morgan('combined'));
 app.use('/(.well-known/)?apple-app-site-association', artsyEigenWebAssociation);
+app.get('/ping', function (req, res) { res.send('pong'); });
 app.use(function(req, res, next) {
   res.setHeader('Strict-Transport-Security', 'max-age=0');
   res.redirect(301, 'https://www.artsy.net' + req.url);
 });
-http.createServer(app).listen(port);
-if (ssl) {
-  https.createServer({
-    key: fs.readFileSync('/srv/www/artsy_wwwify/shared/config/ssl.key').toString(),
-    cert: fs.readFileSync('/srv/www/artsy_wwwify/shared/config/ssl.crt').toString()
-  }, app).listen(443);
-}
+module.exports = http.createServer(app).listen(3000);
