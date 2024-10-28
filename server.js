51b227e2ec6847ef1e7dc0a1cba7ba8bd257113c
brainbicycle <brian.beckerle@artsymail.com>
2024-10-28 18:02:05 -0400

    remove routes for deeplink and verification files

--- server.js
+++ server.js
@@ -1,34 +1,8 @@
-var path = require('path');
-var artsyEigenWebAssociation = require('@artsy/eigen-web-association');
-var iosVerificationFileName = path.resolve(__dirname, 'apple-developer-domain-association.txt');
-var androidVerificationFileName = path.resolve(__dirname, 'assetlinks.json');
-
 var express = require('express');
 var http = require('http');
 var morgan = require('morgan');
 var app = express();
 app.use(morgan('combined'));
-app.use('/(.well-known/)?apple-app-site-association', artsyEigenWebAssociation);
-
-app.get('/.well-known/apple-developer-domain-association.txt', function (req, res) {
-  res.sendFile(iosVerificationFileName, { 'headers': { 'Content-Type': 'text/plain' } }, function (error) {
-    if (error) {
-      console.log(error);
-      code = error.status >= 100 && error.status < 600 ? error.status : 500
-      response.status(code).end();
-    }
-  })
-})
-
-app.get('/.well-known/assetlinks.json', function (req, res) {
-  res.sendFile(androidVerificationFileName, { 'headers': { 'Content-Type': 'application/json' } }, function (error) {
-    if (error) {
-      console.log(error);
-      code = error.status >= 100 && error.status < 600 ? error.status : 500
-      response.status(code).end();
-    }
-  })
-})
 
 app.get('/ping', function (req, res) { res.send('pong'); });
 app.use(function (req, res, next) {
